a1b214b9c44a6e503187b30be72f3bd456e45312
BenMosheSapo <bensapo@gmail.com>
2016-02-18 08:37:13 +0200

    Original FIzz Buzz Program

---
+++ ConsoleApplication2/FizzBuzz.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int i;
+	for (i = 1; i <= 20; i++){
+		if (i % 3 == 0){
+			cout << "Fizz ";
+		}
+		if (i % 5 == 0){
+			cout << "Buzz";
+		}
+		if (!(i % 3 == 0 || i % 5 == 0)){
+			cout << i;
+		}
+		cout << "" << endl;
+	}
+}
