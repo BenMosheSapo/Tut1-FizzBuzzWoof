741ebed0bea9d48f52333f1becc4c5e841be1ee7
BenMosheSapo <bensapo@gmail.com>
2016-02-18 08:39:48 +0200

    Increased Count and Added Woof when divisible by 7

--- ConsoleApplication2/FizzBuzz.cpp
+++ ConsoleApplication2/FizzBuzz.cpp
@@ -4,14 +4,17 @@ using namespace std;
 
 int main(){
 	int i;
-	for (i = 1; i <= 20; i++){
+	for (i = 1; i <= 50; i++){
 		if (i % 3 == 0){
 			cout << "Fizz ";
 		}
 		if (i % 5 == 0){
 			cout << "Buzz";
 		}
-		if (!(i % 3 == 0 || i % 5 == 0)){
+		if (i % 7 == 0){
+			cout << "Woof";
+		}
+		if (!(i % 3 == 0 || i % 5 == 0 || i%7==0)){
 			cout << i;
 		}
 		cout << "" << endl;
